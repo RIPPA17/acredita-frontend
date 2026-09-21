7ecbb89979ba3e3ba36975173e39756f88b0e15c
RIPPA17 <roig.ppr@gmail.com>
2026-09-21 12:55:21 -0300

    db: retirar alias de timestamp de migración

--- supabase/migrations/20260921004000_payment_annulled_replacement_v1.sql
+++
@@ -1,7 +0,0 @@
-alter table public.payment_cases
-  drop constraint if exists payment_cases_accreditation_id_period_start_period_end_key;
-
-drop index if exists public.payment_cases_active_period_unique;
-create unique index payment_cases_active_period_unique
-  on public.payment_cases(accreditation_id,period_start,period_end)
-  where status <> 'anulado';
